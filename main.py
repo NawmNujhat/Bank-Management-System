76e6c0628d190308c265d4e1f395d995f9694248
NawmNujhat <nujhat2307@gmail.com>
2020-08-22 01:40:36 +0600

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
 import string
 from luhn import *
+import sqlite3
+from sqlite3 import Error
+
 global accounts
-accounts=dict()
+
+
 
 
 def luhn_algorithm(account):
@@ -39,36 +43,54 @@ def create_account():
 def create_pin():
     pin=str(random.randint(1000,9999))
     return pin
+
+
+
+
 def log_account():
     print("\n")
     print("Enter your card number:")
-    acid=int(input())
+    acid=input()
     print("Enter your PIN:")
     pid=input()
-    if(accounts.get(acid) is not None):
-         if(accounts[acid]==pid):
-             while(True):
+    i=0
+
+    cur.execute("SELECT * FROM card WHERE number=? And pin=?", (acid, pid))
+    row=cur.fetchall()
+    if(len(row)==0):
+        print("\n")
+        print("Wrong card number or PIN!")
+    else:
+        while (True):
+            i=i+1
+            if(i==1):
                 print("\n")
                 print("You have successfully logged in!")
-                print("\n")
-                print("1. Balance")
-                print("2. Log out")
-                print("0. Exit")
-                a=int(input())
-                if(a==1):
-                    print("Balance: 0")
-                elif(a==2):
-                    print("You have successfully logged out!")
-                    break;
-                elif(a==0):
-                    print("Bye!")
-                    exit()
-         else:
-             print("\n")
-             print("Wrong card number or PIN!")
-    elif(accounts.get(acid) is None):
-        print("Wrong card number or PIN!")
+            print("\n")
+            print("1. Balance")
+            print("2. Log out")
+            print("0. Exit")
+
+            a = int(input())
+            if (a == 1):
+                cur.execute("SELECT balance FROM card WHERE number=? And pin=?", (acid, pid))
+                row = cur.fetchall()
+                print("Balance: 0")
+            elif (a == 2):
+                print("You have successfully logged out!")
+                break
+            elif (a == 0):
+                print("Bye!")
+                exit()
+
+
 def main():
+  global conn,cur
+  conn = sqlite3.connect('card.s3db')
+  cur=conn.cursor()
+  createTable="Create Table IF NOT EXISTS card(id int primary key,number text,pin text,balance INTEGER DEFAULT 0)"
+  cur.execute(createTable)
+  conn.commit()
   i=0
   while(True):
       i=i+1
@@ -81,14 +103,20 @@ def main():
 
       op=int(input())
       if(op==1):
-          account=create_account()
-          pin=create_pin()
+          account_number=create_account()
+          PIN=create_pin()
           print("\n")
           print("Your card has been created\nYour card number:")
-          print(account)
+          print(account_number)
+          account_number=str(account_number)
           print("Your card PIN:")
-          print(pin)
-          accounts[account]=pin
+          print(PIN)
+          account_number=str(account_number)
+          id=random.randint(10000,99999)
+          cur.execute("INSERT INTO card (id,number, pin,balance) VALUES (?, ?, ?, ?)", (id,account_number, PIN,0))
+          conn.commit()
+
+
       elif(op==2):
           log_account()
       elif(op==0):
